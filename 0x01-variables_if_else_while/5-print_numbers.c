f4cd1c7a17e6f73d10bb840b4a3eab4bd8cc3abe
Ahmed Fkri <ahmedfkri@hotmail.com>
2023-03-18 02:58:43 -0700

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: single digits of base 10
+ *
+ * Return: 0 succss
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%i", i);
+	printf('\n');
+}
