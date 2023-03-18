e09e39b037a2e7b96e46a61b05237c4d507b3331
Ahmed Fkri <ahmedfkri@hotmail.com>
2023-03-18 03:27:03 -0700

    nums,

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: combinations
+ *
+ * Return: 0 success
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 0);
+
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
